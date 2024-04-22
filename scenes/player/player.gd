69744fbf6d87e909ba20bbb0abcb0eb3613e1dd9
elliottdombrowski <elliottroyal@me.com>
2024-04-22 13:27:30 -0500

    updates

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,41 +1,49 @@
-extends CharacterBody2D
+extends CharacterBody2D 
 
-@export var RUN_SPEED    : int = 180
-@export var WALK_SPEED   : int = 100
-@export var CROUCH_SPEED : int = 80
-@export var JUMP_FORCE   : int = 255
-@export var GRAVITY      : int = 900
+@export var LAST_HELD_DIRECTION : int = 1
 
-@export var IS_IDLE      : bool = true
-@export var IS_WALKING   : bool = false
-@export var IS_RUNNING   : bool = false
-@export var IS_CROUCHED  : bool = false
+@export var ACCELERATION_SMOOTHING : int = 25
+
+@export var RUN_SPEED              : int = 180
+@export var WALK_SPEED             : int = 100
+@export var CROUCH_SPEED           : int = 80
+@export var ROLL_SPEED             : int = 200
+@export var JUMP_FORCE             : int = 255
+@export var GRAVITY                : int = 900
+
+@export var IS_IDLE                : bool = true
+@export var IS_WALKING             : bool = false
+@export var IS_RUNNING             : bool = false
+@export var IS_CROUCHED            : bool = false
+@export var IS_ROLLING             : bool = false
 
 
 func _physics_process(delta):
-	handle_player_movement()
+	handle_player_movement(delta)
 
 
-func handle_player_movement():
-	var direction   = Input.get_axis("move_left", "move_right")
-	var is_running  = player_is_running()
+func handle_player_movement(delta):
+	var direction = get_x_movement()
+	if direction != 0: LAST_HELD_DIRECTION = direction # Update last held direction
+
 	var is_crouched = player_is_crouched()
-	
+
 	if direction:
 		IS_IDLE    = false
-		IS_WALKING = true
+		IS_RUNNING = true
+
 		if is_crouched:
 			velocity.x = direction * CROUCH_SPEED
 			$AnimatedSprite2D.play("crouch_walk")
-		elif is_running:
-			velocity.x = direction * RUN_SPEED
-			$AnimatedSprite2D.play("run")
 		else:
-			velocity.x = direction * WALK_SPEED
-			$AnimatedSprite2D.play("walk")
+			velocity.x = direction * RUN_SPEED
+			if (direction > 0 && direction < 1) or (direction < 0 && direction > -1):
+				$AnimatedSprite2D.play("walk")
+			else: $AnimatedSprite2D.play("run")
 	else:
-		IS_WALKING = false
+		IS_RUNNING = false
 		IS_IDLE    = true
+
 		velocity.x = 0
 		if is_crouched: $AnimatedSprite2D.play("crouch_idle")
 		else:           $AnimatedSprite2D.play("idle")
@@ -55,18 +63,14 @@ func handle_player_movement():
 	move_and_slide()
 
 
-func get_player_facing_direction(direction):
-	if   direction ==  1: $AnimatedSprite2D.flip_h = false
-	elif direction == -1: $AnimatedSprite2D.flip_h = true
+func get_x_movement():
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	return x_movement
 
-# Hold to Run
-func player_is_running():
-	if Input.is_action_pressed("hold_sprint"): # and is_on_floor():
-		IS_CROUCHED = false
-		IS_RUNNING  = true
-	else:
-		IS_RUNNING  = false
-	return IS_RUNNING
+
+func get_player_facing_direction(direction):
+	if   direction > 0: $AnimatedSprite2D.flip_h = false
+	elif direction < 0: $AnimatedSprite2D.flip_h = true
 
 
 # Toggle crouch
@@ -74,3 +78,14 @@ func player_is_crouched():
 	if Input.is_action_just_pressed("toggle_crouch"): # and is_on_floor():
 		IS_CROUCHED = !IS_CROUCHED
 	return IS_CROUCHED
+
+
+# Press to roll
+func player_is_rolling():
+	if Input.is_action_just_pressed("roll"): # and is_on_floor():
+		IS_CROUCHED = false
+		IS_WALKING  = false
+		IS_RUNNING  = false
+		IS_ROLLING  = true
+	else: IS_ROLLING = false
+	return IS_ROLLING
