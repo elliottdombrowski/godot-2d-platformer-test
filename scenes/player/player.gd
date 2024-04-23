8e2c969b1754ff6eecc14c69754135f6af923536
elliottdombrowski <elliottroyal@me.com>
2024-04-22 22:03:31 -0500

    fix double jump animation bug

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,19 +8,21 @@ extends CharacterBody2D
 @export var WALK_SPEED             : int = 100
 @export var CROUCH_SPEED           : int = 80
 @export var ROLL_SPEED             : int = 200
-@export var JUMP_FORCE             : int = 255
-@export var GRAVITY                : int = 1100
+@export var JUMP_FORCE             : int = 300
+@export var GRAVITY                : int = 900
 
 @export var IS_IDLE                : bool = true
 @export var IS_WALKING             : bool = false
 @export var IS_RUNNING             : bool = false
 @export var IS_JUMPING             : bool = false
+@export var CAN_DOUBLE_JUMP        : bool = false
 @export var IS_CROUCHED            : bool = false
 @export var IS_ROLLING             : bool = false
 @export var IS_ATTACKING           : bool = false
 
 func _physics_process(delta):
 	var direction = get_x_movement()
+	get_player_facing_direction(direction)
 	handle_player_movement(delta, direction)
 	handle_player_attack()
 
@@ -28,8 +30,9 @@ func _physics_process(delta):
 func handle_player_movement(delta, direction):
 	if direction != 0: LAST_HELD_DIRECTION = direction # Update last held direction
 
-	var is_crouched = player_is_crouched()
-	
+	var is_crouched : bool = player_is_crouched()
+	var is_grounded : bool = player_is_grounded()
+
 	if IS_ATTACKING: return
 	if direction:
 		IS_IDLE    = false
@@ -37,27 +40,30 @@ func handle_player_movement(delta, direction):
 
 		if is_crouched:
 			velocity.x = direction * CROUCH_SPEED
-			if !IS_JUMPING:
-				$AnimatedSprite2D.play("crouch_walk") 
+			play_animation("crouch_walk")
 		else:
 			velocity.x = direction * RUN_SPEED
 			if (direction > 0 && direction < 1) or (direction < 0 && direction > -1):
-				$AnimatedSprite2D.play("walk")
-			else: $AnimatedSprite2D.play("run")
+				play_animation("walk")
+			else: play_animation("run")
 	else:
 		IS_RUNNING = false
 		IS_IDLE    = true
 
 		velocity.x = 0
-		if is_crouched: $AnimatedSprite2D.play("crouch_idle")
-		else:           $AnimatedSprite2D.play("idle")
-	
-	# Flip player
-	get_player_facing_direction(direction)
+		if is_crouched: play_animation("crouch_idle")
+		else:           play_animation("idle")
 	
 	# Jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y -= JUMP_FORCE
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y -= JUMP_FORCE
+			CAN_DOUBLE_JUMP = true
+			play_animation("jump")
+		if !is_on_floor() and CAN_DOUBLE_JUMP and Input.is_action_just_pressed("jump"):
+			velocity.y -= JUMP_FORCE
+			CAN_DOUBLE_JUMP = false
+			play_animation("double_jump")
 
 	# Gravity
 	if not is_on_floor():
@@ -71,14 +77,12 @@ func handle_player_attack():
 
 	if Input.is_action_just_pressed("basic_attack"):
 		var animation = "sword_stab" if randf() < 0.4 else "sword_slash" 
-
 		IS_ATTACKING = true
 		$AnimatedSprite2D.play(animation)
-		return
+
 	if Input.is_action_just_pressed("heavy_attack"):
 		IS_ATTACKING = true
 		$AnimatedSprite2D.play("sword_heavy_attack")
-		return
 
 
 func get_x_movement():
@@ -98,15 +102,16 @@ func player_is_crouched():
 	return IS_CROUCHED
 
 
-# Press to roll
-func player_is_rolling():
-	if Input.is_action_just_pressed("roll") and is_on_floor():
-		IS_CROUCHED = false
-		IS_WALKING  = false
-		IS_RUNNING  = false
-		IS_ROLLING  = true
-	else: IS_ROLLING = false
-	return IS_ROLLING
+func player_is_grounded():
+	IS_JUMPING = !is_on_floor()
+	return IS_JUMPING
+
+
+func play_animation(anim_str):
+	match anim_str:
+		"jump":            $AnimatedSprite2D.play(anim_str)
+		"double_jump":     $AnimatedSprite2D.play(anim_str)
+		_: if !IS_JUMPING: $AnimatedSprite2D.play(anim_str)
 
 
 func _on_animated_sprite_2d_animation_finished():
